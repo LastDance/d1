e1bfed5534c00b2f2c3e551eda62bc8e16f2a67c
Yi Ding <dingyi216@gmail.com>
2013-07-07 14:19:25 +0800

    modified:   schema/YT_schema modified:   schema/dms.sql 	modified:   src/main/resources/hibernate.cfg.xml

--- schema/dms.sql
+++ schema/dms.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
+Source Server         : mysql
 Source Server Version : 50611
 Source Host           : localhost:3306
 Source Database       : dms
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50611
 File Encoding         : 65001
 
-Date: 2013-07-07 14:02:02
+Date: 2013-07-06 23:36:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,16 +20,14 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `material_detail`;
 CREATE TABLE `material_detail` (
-  `matl_id` int(11) NOT NULL AUTO_INCREMENT,
-  `matl_tkl_id` int(11) NOT NULL,
+  `matl_id` int(11) NOT NULL,
   `matl_attr1` varchar(20) DEFAULT NULL,
   `matl_attr2` varchar(20) DEFAULT NULL,
   `matl_attr3` varchar(20) DEFAULT NULL,
   `matl_attr4` varchar(20) DEFAULT NULL,
-  `matl_name` varchar(20) DEFAULT NULL,
+  `matl_nbr` varchar(20) DEFAULT NULL,
   `matl_desc` varchar(40) DEFAULT NULL,
-  `matl_ctg` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`matl_id`)
+  `matl_ctg` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -41,7 +39,7 @@ CREATE TABLE `material_detail` (
 -- ----------------------------
 DROP TABLE IF EXISTS `order_line`;
 CREATE TABLE `order_line` (
-  `ordl_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ordl_id` int(11) NOT NULL,
   `ordl_ord_id` int(11) NOT NULL,
   `ordl_nbr` int(11) NOT NULL,
   `ordl_line_item` varchar(40) NOT NULL,
@@ -54,14 +52,18 @@ CREATE TABLE `order_line` (
   `ordl_cmt` text,
   `ordl_active` bit(1) NOT NULL,
   PRIMARY KEY (`ordl_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of order_line
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `order_master`
 -- ----------------------------
 DROP TABLE IF EXISTS `order_master`;
 CREATE TABLE `order_master` (
-  `ord_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ord_id` int(11) NOT NULL,
   `ord_prefix` varchar(20) NOT NULL,
   `ord_cust_nbr` varchar(40) DEFAULT NULL,
   `ord_cust` varchar(40) NOT NULL,
@@ -72,16 +74,19 @@ CREATE TABLE `order_master` (
   `ord_status` varchar(40) NOT NULL,
   `ord_cmt` text,
   PRIMARY KEY (`ord_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of order_master
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `process_appr`
 -- ----------------------------
 DROP TABLE IF EXISTS `process_appr`;
 CREATE TABLE `process_appr` (
-  `pa_id` int(11) NOT NULL AUTO_INCREMENT,
-  `pa_appr` varchar(40) NOT NULL,
-  PRIMARY KEY (`pa_id`)
+  `pa_id` int(11) DEFAULT NULL,
+  `pa_appr` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -93,7 +98,7 @@ CREATE TABLE `process_appr` (
 -- ----------------------------
 DROP TABLE IF EXISTS `review_detail`;
 CREATE TABLE `review_detail` (
-  `rvw_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rvw_id` int(11) NOT NULL,
   `rvw_group` int(11) NOT NULL,
   `rvw_ent_by` varchar(40) NOT NULL,
   `rvw_ent_date` date DEFAULT NULL,
@@ -106,24 +111,25 @@ CREATE TABLE `review_detail` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for `task_line`
+-- Table structure for `task_detail`
 -- ----------------------------
-DROP TABLE IF EXISTS `task_line`;
-CREATE TABLE `task_line` (
-  `tkl_id` int(11) NOT NULL AUTO_INCREMENT,
-  `tkl_tk_id` int(11) NOT NULL,
-  `tkl_part` varchar(20) NOT NULL,
-  `tkl_desc` varchar(80) DEFAULT NULL,
-  `tkl_matl_id` int(11) NOT NULL,
-  `tkl_qty_set` double NOT NULL,
-  `tkl_qty_per_set` double NOT NULL,
-  `tkl_appr` varchar(40) NOT NULL,
-  `tkl_cmt` text,
-  PRIMARY KEY (`tkl_id`)
+DROP TABLE IF EXISTS `task_detail`;
+CREATE TABLE `task_detail` (
+  `tkd_id` int(11) NOT NULL,
+  `tkd_tk_id` int(11) NOT NULL,
+  `tkd_part` varchar(20) NOT NULL,
+  `tkd_desc` varchar(80) DEFAULT NULL,
+  `tkd_cust_part` varchar(40) DEFAULT NULL,
+  `tkd_matl_id` int(11) NOT NULL,
+  `tkd_qty_set` double NOT NULL,
+  `tkd_qty_per_set` double NOT NULL,
+  `tkd_appr` varchar(40) NOT NULL,
+  `tkd_cmt` text,
+  PRIMARY KEY (`tkd_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of task_line
+-- Records of task_detail
 -- ----------------------------
 
 -- ----------------------------
@@ -131,7 +137,7 @@ CREATE TABLE `task_line` (
 -- ----------------------------
 DROP TABLE IF EXISTS `task_master`;
 CREATE TABLE `task_master` (
-  `tk_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tk_id` int(11) NOT NULL,
   `tk_ordl_id` int(11) NOT NULL,
   `tk_product` varchar(40) NOT NULL,
   `tk_desc` varchar(40) DEFAULT NULL,
@@ -153,14 +159,14 @@ CREATE TABLE `task_master` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `user_name` varchar(40) NOT NULL,
   `user_passwd` varchar(40) NOT NULL,
   `user_email` varchar(80) DEFAULT NULL,
   `user_telephone` varchar(20) NOT NULL,
   `user_qq` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
